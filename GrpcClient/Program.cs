39e005d55a8e57243d776dacc2de6dc8bbb20b11
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2021-12-28 22:06:07 +0200

    initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeter;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        private const string GrpcServerUrl = "https://localhost:5001";
+        
+        static async Task Main(string[] args)
+        {
+            // Return `true` to allow certificates that are untrusted/invalid
+            var httpHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback =
+                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+
+            using var channel = GrpcChannel.ForAddress(GrpcServerUrl, new GrpcChannelOptions{ HttpHandler = httpHandler});
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply =  await client.SayHelloAsync(new HelloRequest
+            {
+                Name = "GrpcClient"
+            });
+            
+            Console.WriteLine("Greeting: " + reply.Message);
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
