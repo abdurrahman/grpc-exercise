d19ef1df74930f99a161c8c7b9c3d6c4b9940115
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2022-01-13 22:04:17 +0200

    implement exchange rates service (wip)

--- src/GrpcServer/Services/ExchangeRatesImpl.cs
+++ src/GrpcServer/Services/ExchangeRatesImpl.cs
@@ -1,15 +1,32 @@
+using System.Net.Http;
+using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using System.Xml;
 
-namespace Exchangerates //GrpcServer.Services
+namespace GrpcServer.Services
 {
-    public class ExchangeRatesImpl : Exchangerates.ExchangeRatesBase
+    public class ExchangeRatesImpl : ExchangeRates.ExchangeRatesBase
     {
         private ILogger _logger;
         
         public ExchangeRatesImpl(ILogger<ExchangeRatesImpl> logger)
         {
             _logger = logger;
+        }
+
+        public override async Task<ExchangeRatesResponse> GetExchangeRates(ExchangeRatesRequest request, 
+	        ServerCallContext context)
+        {
+	        var httpClient = new HttpClient();
+	        var stream = await httpClient.GetStreamAsync("https://www.tcmb.gov.tr/kurlar/today.xml");
+            var xmlDoc = new XmlDocument();
+            
+            xmlDoc.Load(stream);
+
+            // xmlDoc.DocumentElement.SelectSingleNode("/Tarih_Date/");
+
+            return await base.GetExchangeRates(request, context);
         }
     }
 }
