0b6fbca7b674f4abd2b7a99923d4976764cf4812
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2022-01-14 23:57:22 +0200

    update exchange rates service w/ repeated response

--- src/GrpcServer/Services/ExchangeRatesImpl.cs
+++ src/GrpcServer/Services/ExchangeRatesImpl.cs
@@ -28,18 +28,29 @@ namespace GrpcServer.Services
 
             var response = new ExchangeRatesResponse();
             var exchangeRateList = xmlDoc.LastChild; // Tarih_Date
+            var exchangeRates = new List<ExchangeRate>();
             foreach (XmlNode node in exchangeRateList)
             {
-                if (node.Attributes["CurrencyCode"].Value == request.CurrencyCode)
+                // if (node.Attributes["CurrencyCode"].Value == request.CurrencyCode)
+                // {
+                //     response.Unit = Convert.ToInt32(node.ChildNodes[0]?.InnerText);
+                //     response.Currency = node.ChildNodes[2]?.InnerText;
+                //     response.CurrencyCode = node.Attributes["CurrencyCode"]?.Value;
+                //     response.ForexBuying = Convert.ToDouble(node.ChildNodes[3]?.InnerText);
+                //     response.ForexSelling = Convert.ToDouble(node.ChildNodes[4]?.InnerText);
+                // }
+                
+                exchangeRates.Add(new ExchangeRate
                 {
-                    response.Unit = Convert.ToInt32(node.ChildNodes[0]?.InnerText);
-                    response.Currency = node.ChildNodes[2]?.InnerText;
-                    response.CurrencyCode = node.Attributes["CurrencyCode"]?.Value;
-                    response.ForexBuying = Convert.ToDouble(node.ChildNodes[3]?.InnerText);
-                    response.ForexSelling = Convert.ToDouble(node.ChildNodes[4]?.InnerText);
-                }
+                    Unit = Convert.ToInt32(node.ChildNodes[0]?.InnerText),
+                    Currency = node.ChildNodes[2]?.InnerText,
+                    CurrencyCode = node.Attributes["CurrencyCode"]?.Value,
+                    ForexBuying = Convert.ToDouble(node.ChildNodes[3]?.InnerText),
+                    ForexSelling = Convert.ToDouble(node.ChildNodes[4]?.InnerText)
+                });
             }
 
+            response.ExchangeRate.AddRange(exchangeRates);
             return response;
         }
     }
