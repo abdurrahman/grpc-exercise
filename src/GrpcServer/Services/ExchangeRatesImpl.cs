26133bf1e3db1745849419b21a3e8c7dc2659da4
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2022-01-16 22:20:48 +0200

    fix client-side single response

--- src/GrpcServer/Services/ExchangeRatesImpl.cs
+++ src/GrpcServer/Services/ExchangeRatesImpl.cs
@@ -31,23 +31,19 @@ namespace GrpcServer.Services
             var exchangeRates = new List<ExchangeRate>();
             foreach (XmlNode node in exchangeRateList)
             {
-                // if (node.Attributes["CurrencyCode"].Value == request.CurrencyCode)
-                // {
-                //     response.Unit = Convert.ToInt32(node.ChildNodes[0]?.InnerText);
-                //     response.Currency = node.ChildNodes[2]?.InnerText;
-                //     response.CurrencyCode = node.Attributes["CurrencyCode"]?.Value;
-                //     response.ForexBuying = Convert.ToDouble(node.ChildNodes[3]?.InnerText);
-                //     response.ForexSelling = Convert.ToDouble(node.ChildNodes[4]?.InnerText);
-                // }
-                
-                exchangeRates.Add(new ExchangeRate
+                if (node.Attributes?["CurrencyCode"]?.Value == request.CurrencyCode)
                 {
-                    Unit = Convert.ToInt32(node.ChildNodes[0]?.InnerText),
-                    Currency = node.ChildNodes[2]?.InnerText,
-                    CurrencyCode = node.Attributes["CurrencyCode"]?.Value,
-                    ForexBuying = Convert.ToDouble(node.ChildNodes[3]?.InnerText),
-                    ForexSelling = Convert.ToDouble(node.ChildNodes[4]?.InnerText)
-                });
+                    exchangeRates.Add(new ExchangeRate
+                    {
+                        Unit = Convert.ToInt32(node.ChildNodes[0]?.InnerText),
+                        Currency = node.ChildNodes[2]?.InnerText,
+                        CurrencyCode = node.Attributes?["CurrencyCode"]?.Value,
+                        ForexBuying = Convert.ToDouble(node.ChildNodes[3]?.InnerText),
+                        ForexSelling = Convert.ToDouble(node.ChildNodes[4]?.InnerText)
+                    });
+                    
+                    break;
+                }
             }
 
             response.ExchangeRate.AddRange(exchangeRates);
